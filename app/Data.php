9c602b09289ca21278d9fd23cf6258d021bcec0f
mluo20 <mluo20@pascack.org>
2019-01-21 16:56:46 -0500

    thing

--- app/Data.php
+++ app/Data.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Data extends Model
 {
-    
+    protected $fillable = [
+        'match_num', 'team_num', 'alliance', 'starting_point', 'driving_sandstorm_type', 'rocket_hatches', 'rocket_balls', 'cargo_hatches',
+        'cargo_balls', 'end_position', 'red_score', 'blue_score', 'end_score'
+    ];
 }
--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -6,5 +6,58 @@ use Illuminate\Http\Request;
 
 class DataController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $shares = Share::all();
+
+        // return view('shares.index', compact('shares'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
