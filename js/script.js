84feb4b4b298556d259fe5242b48af5b3c240627
Matt Elek Harris <matt@elekharris.com>
2019-01-23 19:07:06 -0500

    Added form fields

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@ var stepper = null;
 document.addEventListener('DOMContentLoaded', function () {
   stepper = new Stepper(document.querySelector('.bs-stepper'))
 })
+
+function plus(countName){
+  var count = countName.value;
+  count++;
+  if (count >= 3) {
+    count = 3;
+  }
+  countName.value = count;
+}
+
+function minus(countName){
+  var count = countName.value;
+  count--;
+  if (count <= 0) {
+    count = 0;
+  }
+  countName.value = count;
+}
